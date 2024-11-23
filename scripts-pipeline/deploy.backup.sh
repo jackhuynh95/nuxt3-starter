6c6497c0c9302c1235d09e338e897cc2db0d2e3e
Jack Huynh <kietdien88@gmail.com>
2024-11-23 12:53:37 +0700

    deploy.sh (clean up) [REVAMPED]

--- scripts-pipeline/deploy.backup.sh
+++ scripts-pipeline/deploy.backup.sh
@@ -8,10 +8,8 @@ DOCKER_NAME="nuxt-starter"
 ORGANIZATION_NAME="jackhuynh" # Your organization name
 REPOSITORY_NAME="${ORGANIZATION_NAME}/${DOCKER_NAME}" # Updated ECR repository name
 
-# Get the latest version of the Docker image
-# VERSION=$(docker images ${REPOSITORY_NAME} | awk 'NR>1 {print $2}' | tail -1)
-# VERSION=$(date +%Y%m%d%H%M%S)  # Using timestamp as version
-VERSION="latest" # Using latest version
+# Use 'latest' version
+VERSION="latest"
 
 CUSTOM_HOST=""
 CUSTOM_EMAIL=""
@@ -22,19 +20,19 @@ docker login -u jackhuynh -p $PASSWORD
 # Pull the Docker image to ECR
 docker pull ${REPOSITORY_NAME}:${VERSION}
 
-# Run docker system prune to clean up unused resources
-# docker system prune -af (Open this line to clean up unused resources)
-
 # Stop and remove the existing container
 docker stop ${DOCKER_NAME} || true
-# docker image rm ${DOCKER_NAME} || true (Open this line to clean up this image)
+docker rm ${DOCKER_NAME} || true  # Ensure the container is removed
 
 # Run the new container
-# docker run --rm -d --name ${DOCKER_NAME} -p 3000:80 ${REPOSITORY_NAME}:${VERSION}
 docker run --rm -d --name ${DOCKER_NAME} -p 3000:80 ${REPOSITORY_NAME}:${VERSION} \
   --env "VIRTUAL_HOST=${CUSTOM_HOST}" \
   --env "VIRTUAL_PORT=3000" \
   --env "LETSENCRYPT_HOST=${CUSTOM_HOST}" \
   --env "LETSENCRYPT_EMAIL=${CUSTOM_EMAIL}"
+
+# Clean up unused resources (optional)
+# Uncomment the following line if you want to clean up after the deployment
+# docker system prune -af
 
 echo "Deployment to EC2 completed successfully."
--- scripts-pipeline/deploy.sh
+++ scripts-pipeline/deploy.sh
@@ -25,17 +25,21 @@ ssh -o StrictHostKeyChecking=no -v $EC2_USER@$EC2_INSTANCE_IP << 'EOF'
 
   # Pull the Docker image to ECR
   docker pull ${REPOSITORY_NAME}:${VERSION}
+
   # Stop and remove the existing container
   docker stop ${DOCKER_NAME} || true
-  # docker rm ${DOCKER_NAME} || true
+  docker rm ${DOCKER_NAME} || true  # Ensure the container is removed
 
   # Run the new container
-  # docker run --rm -d --name ${DOCKER_NAME} -p 3000:80 ${REPOSITORY_NAME}:${VERSION}
   docker run --rm -d --name ${DOCKER_NAME} -p 3000:80 ${REPOSITORY_NAME}:${VERSION} \
     --env "VIRTUAL_HOST=${CUSTOM_HOST}" \
     --env "VIRTUAL_PORT=3000" \
     --env "LETSENCRYPT_HOST=${CUSTOM_HOST}" \
     --env "LETSENCRYPT_EMAIL=${CUSTOM_EMAIL}"
+
+  # Clean up unused resources (optional)
+  # Uncomment the following line if you want to clean up after the deployment
+  # docker system prune -af
 
   echo "Deployment to EC2 completed successfully."
 EOF
