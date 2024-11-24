ba5a552ca7de64f4885fe4cc178aca342cf349a6
Jack Huynh <kietdien88@gmail.com>
2024-11-24 17:19:16 +0700

    env.sh - reuse environment variable - version

--- scripts-pipeline/env.sh
+++ scripts-pipeline/env.sh
@@ -6,6 +6,6 @@ export ORGANIZATION_NAME="jackhuynh" # Your organization name
 export REPOSITORY_NAME="${ORGANIZATION_NAME}/${DOCKER_NAME}" # Updated Docker Hub repository name
 
 # Get the latest version of the Docker image
-# VERSION=$(docker images ${REPOSITORY_NAME} | awk 'NR>1 {print $2}' | tail -1)
+VERSION=$(docker images ${REPOSITORY_NAME} | awk 'NR>1 {print $2}' | tail -1)
 # VERSION=$(date +%Y%m%d%H%M%S)  # Using timestamp as version
-VERSION="latest" # Using latest version
+# VERSION="latest" # Using latest version
