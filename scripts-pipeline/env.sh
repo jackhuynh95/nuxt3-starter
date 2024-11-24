4a2fb99d6d3b781ae56409980d480fdc2f976c07
Jack Huynh <kietdien88@gmail.com>
2024-11-24 17:27:13 +0700

    env.sh - reuse environment variable - Generate version based on semantic versioning

--- scripts-pipeline/env.sh
+++ scripts-pipeline/env.sh
@@ -5,7 +5,11 @@ export DOCKER_NAME="nuxt-starter"
 export ORGANIZATION_NAME="jackhuynh" # Your organization name
 export REPOSITORY_NAME="${ORGANIZATION_NAME}/${DOCKER_NAME}" # Updated Docker Hub repository name
 
-# Get the latest version of the Docker image
-# VERSION=$(docker images ${REPOSITORY_NAME} | awk 'NR>1 {print $2}' | tail -1)
-VERSION=$(date +%Y%m%d%H%M%S)  # Using timestamp as version
-# VERSION="latest" # Using latest version
+# Generate version based on semantic versioning
+# This could be read from a VERSION file or set manually
+PREFIX_VERSION="1.0.0"  # Update this as needed for your releases
+
+# Optionally, append a timestamp for uniqueness
+SUFFIX_TIMESTAMP=$(date +%Y%m%d%H%M%S)
+
+export VERSION="${PREFIX_VERSION}-${SUFFIX_TIMESTAMP}"
